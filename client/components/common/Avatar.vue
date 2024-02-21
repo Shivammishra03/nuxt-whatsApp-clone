213de490475727a8a177f67292dfc44157fe54a9
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-02-21 16:23:03 +0530

    chore: onbaording page setup

---
+++ client/components/common/Avatar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex items-center justify-center">
+        <div v-if="type === 'sm'" class="relative h-10 w-10">
+            <NuxtImg :src="imgSrc" alt="avatar" class="rounded-full"/>
+        </div>
+        <div v-if="type === 'lg'" class="relative h-14 w-14">
+            <NuxtImg :src="imgSrc" alt="avatar" class="rounded-full"/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    type: String,
+    imgSrc: String
+});
+</script>
--- client/pages/onboarding.vue
+++ client/pages/onboarding.vue
@@ -1,5 +1,13 @@
 <script setup>
 // import Input from '~/components/common/input.vue';
+import {useUserStore} from  '~/store/user';
+import Avatar from '~/components/common/Avatar.vue';
+const userInfo = useUserStore();
+
+const name = ref(userInfo?.name ||'');
+const about = ref('');
+const image = ref('/default_avatar.png');
+
 </script>
 <template>
     <div class="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
@@ -10,8 +18,22 @@
         <h2 class="text-2xl">Create Your Profile</h2>
         <div class="flex gap-6 mt-6">
             <div class="flex flex-col items-center justify-center my-5 gap-6">
-                
+                <div class="flex gap-1 flex-col">
+                    <label for="dispalyName" class="text-teal-light text-lg px-1">Display Name</label>
+                    <div>
+                        <input type="text" name="dispalyName" class="bg-input-background text-start focus:outline-none" v-model="name" />
+                    </div>
+                </div>
+                <div class="flex gap-1 flex-col">
+                    <label for="about" class="text-teal-light text-lg px-1">About</label>
+                    <div>
+                        <input type="text" name="about" class="bg-input-background text-start focus:outline-none" v-model="about" />
+                    </div>
+                </div>
             </div>
+        </div>
+        <div>
+            <Avatar type="lg" :img-src="image"/>
         </div>
     </div>
 </template>
